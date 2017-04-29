42c7a275753c960d7fe1c8b1a3afdae11b6bb42f
Mariusz Ozdoba <mariuszozdoba@MacBook-Pro-Mariusz.local>
2017-04-29 19:56:24 +0200

    added km/h output

--- Pace_Calculator.py
+++ Pace_Calculator.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from Tkinter import *
 
 top = Tk()
 top.title("Pace Calculator")
@@ -19,18 +19,15 @@ E2 = Entry(top, bd=5)
 E2.grid(row=3, column=0)
 
 
-def get_minutes
-    print(minutes)
+def get_minutes():
+    minutes = E1.get()
     minutes_to_sec = int(minutes) * 60
-    print(minutes_to_sec)
     seconds = E2.get()
     seconds = int(seconds)
     pace_in_seconds = (minutes_to_sec + seconds) * minute_per_kilometer
     pace_in_seconds = int(pace_in_seconds)
     m, s = divmod(pace_in_seconds, 60)
     E3.insert(0, '{}:{}'.format(m, s))
-
-    print(seconds)
 
 
 def reset():
@@ -48,6 +45,68 @@ L3.grid(row=5, column=0)
 
 E3 = Entry(top, bd=5)
 E3.grid(row=6, column=0)
+
+L4 = Label(top, text="Pace km/h")
+L4.grid(row=7, column=0)
+
+E4 = Entry(top, bd=5)
+E4.grid(row=8, column=0)
+
+
+top.mainloop()
+from Tkinter import *
+
+top = Tk()
+top.title("Pace Calculator")
+
+minute_per_kilometer = 1.609344
+
+L1 = Label(top, text="minutes")
+L1.grid(row=0, column=0)
+
+E1 = Entry(top, bd=5)
+E1.grid(row=1, column=0)
+
+
+L2 = Label(top, text=" seconds")
+L2.grid(row=2, column=0)
+
+E2 = Entry(top, bd=5)
+E2.grid(row=3, column=0)
+
+
+def get_minutes():
+    minutes = E1.get()
+    minutes_to_sec = int(minutes) * 60
+    seconds = E2.get()
+    seconds = int(seconds)
+    pace_in_seconds = (minutes_to_sec + seconds) * minute_per_kilometer
+    pace_in_seconds = int(pace_in_seconds)
+    m, s = divmod(pace_in_seconds, 60)
+    E3.insert(0, '{}:{}'.format(m, s))
+
+
+def reset():
+    E3.delete(0, 'end')
+
+
+B1 = Button(top, text="Enter", command=get_minutes)
+B1.grid(row=4, column=0)
+
+B2 = Button(top, text="Reset", command=reset)
+B2.grid(row=4, column=1)
+
+L3 = Label(top, text="min/mile pace")
+L3.grid(row=5, column=0)
+
+E3 = Entry(top, bd=5)
+E3.grid(row=6, column=0)
+
+L4 = Label(top, text="Pace km/h")
+L4.grid(row=7, column=0)
+
+E4 = Entry(top, bd=5)
+E4.grid(row=8, column=0)
 
 
 top.mainloop()
