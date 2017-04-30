9d1f3434050ad7e2180ddda86df8691a9fa6e699
Mariusz Ozdoba <mariuszozdoba@MacBook-Pro-Mariusz.local>
2017-04-30 13:44:39 +0200

    rounded km/h

--- Pace_Calculator.py
+++ Pace_Calculator.py
@@ -1,10 +1,29 @@
-from Tkinter import *
+from tkinter import *
 
 top = Tk()
 top.title("Pace Calculator")
 
 minute_per_kilometer = 1.609344
 meter_per_second = 3.6
+
+
+def get_minutes():
+    minutes = E1.get()
+    minutes_to_sec = float(minutes) * 60
+    seconds = E2.get()
+    seconds = float(seconds)
+    pace_in_seconds = (minutes_to_sec + seconds) * minute_per_kilometer
+    pace_in_seconds = int(pace_in_seconds)
+    velocity = (1000 / (minutes_to_sec + seconds)) * meter_per_second
+    rounded_velocity = round(velocity, 2)
+    m, s = divmod(pace_in_seconds, 60)
+    E3.insert(0, '{}:{}'.format(m, s))
+    E4.insert(0, rounded_velocity)
+
+
+def reset():
+    E3.delete(0, 'end')
+    E4.delete(0, 'end')
 
 L1 = Label(top, text="minutes")
 L1.grid(row=0, column=0)
@@ -17,24 +36,6 @@ L2.grid(row=2, column=0)
 
 E2 = Entry(top, bd=5)
 E2.grid(row=3, column=0)
-
-
-def get_minutes():
-    minutes = E1.get()
-    minutes_to_sec = float(minutes) * 60
-    seconds = E2.get()
-    seconds = float(seconds)
-    pace_in_seconds = (minutes_to_sec + seconds) * minute_per_kilometer
-    pace_in_seconds = int(pace_in_seconds)
-    velocity = (1000 / (minutes_to_sec + seconds)) * meter_per_second
-    m, s = divmod(pace_in_seconds, 60)
-    E3.insert(0, '{}:{}'.format(m, s))
-    E4.insert(0, velocity)
-
-
-def reset():
-    E3.delete(0, 'end')
-    E4.delete(0, 'end')
 
 B1 = Button(top, text="Enter", command=get_minutes)
 B1.grid(row=4, column=0)
