61db184ce6c2340255154b408c81dbb973bad4ea
Mariusz Ozdoba <mariuszozdoba@MacBook-Pro-Mariusz.local>
2017-04-30 13:55:28 +0200

    modified GUI

--- Pace_Calculator.py
+++ Pace_Calculator.py
@@ -25,34 +25,34 @@ def reset():
     E3.delete(0, 'end')
     E4.delete(0, 'end')
 
-L1 = Label(top, text="minutes")
+L1 = Label(top, text="minutes:")
 L1.grid(row=0, column=0)
 
 E1 = Entry(top, bd=5)
-E1.grid(row=1, column=0)
+E1.grid(row=0, column=1)
 
 L2 = Label(top, text=" seconds")
-L2.grid(row=2, column=0)
+L2.grid(row=1, column=0)
 
 E2 = Entry(top, bd=5)
-E2.grid(row=3, column=0)
+E2.grid(row=1, column=1)
 
 B1 = Button(top, text="Enter", command=get_minutes)
-B1.grid(row=4, column=0)
+B1.grid(row=2, column=0)
 
 B2 = Button(top, text="Reset", command=reset)
-B2.grid(row=4, column=1)
+B2.grid(row=2, column=1)
 
 L3 = Label(top, text="min/mile pace")
-L3.grid(row=5, column=0)
+L3.grid(row=3, column=0)
 
 E3 = Entry(top, bd=5)
-E3.grid(row=6, column=0)
+E3.grid(row=3, column=1)
 
 L4 = Label(top, text="Pace km/h")
-L4.grid(row=7, column=0)
+L4.grid(row=4, column=0)
 
 E4 = Entry(top, bd=5)
-E4.grid(row=8, column=0)
+E4.grid(row=5, column=1)
 
 top.mainloop()
